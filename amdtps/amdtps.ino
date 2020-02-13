f7139ad5beb7d1f1a742dfca28cf956cb7a1069d
kerryeven <41651764+kerryeven@users.noreply.github.com>
2020-02-13 06:54:02 -0500

    add documentation in .ino file - How it works...

--- amdtps/amdtps.ino
+++ amdtps/amdtps.ino
@@ -9,7 +9,7 @@
       (it is used to determine which pin controls the LED)
     - Compile and upload the example to your board with the Arduino "Upload" button
     - In the nRF Connect app look for the device in the "scan" tab. 
-        (By default it is called "Amdtp" but you can change that below)
+        (By default it is called "Amdtp" but you can change that with code)
     - Once the device is found click "connect"
     - The GATT server will load with 4 services:
       - Generic Access
@@ -26,6 +26,17 @@
         (should be the same).
     
 */
+/********************************************************************************************************************
+                 Variables Section - How it all Works
+                  s_Rcvd is declared extern at top of BLE_example_funcs.cpp.  It set by the function bleRxTxReceived
+                  (also defined in BLE_example_funcs.cpp at the end). I have called bleRxTxReceived in amdtps_main.c
+                  at line 368 in function amdtps_write_cback which is called when the chip receives a BLE message.
+
+                  The loop function in this file checks to see if s_Rcvd has a new value (new BLE message received)
+                  and executes something.  That could be a function to control a motor or a servo, etc., etc.  The
+                  first step of whatever is done is to reset s_Rcvd to 100 to stop repeating the commands/function
+                  with every loop..which is VERY often.
+ *******************************************************************************************************************/
 #include "BLE_example.h"
 String s_Rcvd = "100"; // if 100 don't bother checking case statements
 
@@ -39,19 +50,44 @@ void setup() {
   set_led_low();
 
 
-  //
-  // Boot the radio.
-  //
+  /********************************************************************************************************************
+                   Boot the radio
+                    SDK/third_party/exactle/sw/hci/apollo3/hci_drv_apollo3.c
+                    = huge program to handle the ble radio stuff in this file
+   *******************************************************************************************************************/
   HciDrvRadioBoot(0);
 
-  //
-  // Initialize the main ExactLE stack.
-  //
+  /************************************************************************************************
+        Initialize the main ExactLE stack: BLE_example_funcs.cpp
+        - One time timer
+        - timer for handler
+        - dynamic memory buffer
+        - security
+        - HCI host conroller interface
+        - DM device manager software
+        - L2CAP data transfer management
+        - ATT - Low Energy handlers
+        - SMP - Low Energy security
+        - APP - application handlers..global settings..etc
+        - NUS - nordic location services
+
+   ************************************************************************************************/
   exactle_stack_init();
 
-  //
-  // Start the "Amdtp" profile.
-  //
+  /*************************************************************************************************
+      Start the "Amdtp" (AmbiqMicro Data Transfer Protocol) profile. Function in amdtp_main.c
+
+       Register for stack callbacks
+       - Register callback with DM for scan and advertising events with security
+       - Register callback with Connection Manager with client id
+       - Register callback with ATT low energy handlers
+       - Register callback with ATT low enerty connection handlers
+       - Register callback with ATT CCC = client charachteristic configuration array
+       - Register for app framework discovery callbacks
+       - Initialize attribute server database
+       - Reset the device
+
+   ************************************************************************************************/
   AmdtpStart();
 }
 
@@ -60,7 +96,7 @@ if (s_Rcvd != "100") //Check if we have a new message
   {
      switch (s_Rcvd.toInt()) {
       case 0:
-        s_Rcvd = 100;
+        s_Rcvd = 100; //reset or will execute with every loop which is VERY often
         digitalWrite(LED_BUILTIN,0);
         break;
       case 1:
